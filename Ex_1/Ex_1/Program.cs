c209db6d35eee5582bbfeaf3a2242003ec33a8d2
Kawan-Amaral <kawan.melo@aluno.ifsp.edu.br>
2025-06-04 22:19:22 -0300

    Add files via upload

---
+++ Ex_1/Ex_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1;
+        do {
+            Console.Write("Digite um valor positivo: ");
+            valor1 = double.Parse(Console.ReadLine());
+        } while (valor1 <= 0);
+        }
+    }
+}
